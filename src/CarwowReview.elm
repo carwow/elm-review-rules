99a9f22f8a8f7a9c3067d5175c29a6dbfd251df0
Josh Bebbington <josh.bebbington@carwow.co.uk>
2020-10-22 11:04:06 +0000

    Add docs for CarwowReview module

--- src/CarwowReview.elm
+++ src/CarwowReview.elm
@@ -1,9 +1,22 @@
 module CarwowReview exposing (rules)
 
+{-| A set of elm-review rules used across our projects at carwow
+
+
+# Rules
+
+@docs rules
+
+We include rules that help us maintain a clean and tidy codebase, promote good patterns, and codify conventions that we think are important.
+
+-}
+
 import NoUnused.Variables
 import Review.Rule exposing (Rule)
 
 
+{-| Returns a list of elm-review rules that we apply to all of our elm at carwow.
+-}
 rules : List Rule
 rules =
     [ NoUnused.Variables.rule
